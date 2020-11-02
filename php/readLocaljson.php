375f982ecf0bc1cb48c341b9ed2fbe88afc2082f
Pasquale <pasqualesansonna@hotmail.it>
2020-11-02 18:50:46 +0100

    Initial commit

---
+++ php/readLocaljson.php
@@ -0,0 +1,33 @@
+<?php
+
+function queryMyrror($param,$email){
+
+	$json = null;
+	$email = "cat@cat.it";
+	//$email = 'demo@demo.it'; decommentare qui e in script.js per attivare profilo demo
+	
+
+	if (isset($_COOKIE['myrror'])) {
+
+	  	$email = $_COOKIE['myrror'];
+    }
+     	if($param == "today"){
+			$json = file_get_contents('../fileMyrror/today_' . $email . '.json');
+		}else{
+			$json = file_get_contents('../fileMyrror/past_' . $email . '.json');
+		}
+		$result = json_decode($json,true);
+
+		return $result;
+/*
+	}else{
+		echo "<script>location.href='index.html';</script>";
+
+	}
+*/
+	
+
+}
+
+
+?>
